a88b4aa55cd5c2cc4cfc31c5f13929200f6f4412
naimul64 <naimul64@yahoo.com>
2016-10-05 01:57:54 +0600

    school code with original, img and val-score create

--- scriptToCreateJsonDictionary.py
+++ scriptToCreateJsonDictionary.py
@@ -1,4 +1,10 @@
 import random
+from enum import Enum
+
+class CharOption(Enum):
+    Alpha = "Alpha"
+    Numeric = "Numeric"
+    AlphaNumeric = "AlphaNumeric"
 
 noOfScanBySingleScanner = 1000;
 def createNID():
@@ -44,7 +50,7 @@ def getScannerId(index):
     return scannerId;
 
 def getSeq(index):
-    return str(index%noOfScanBySingleScanner + 1);
+    return (index%noOfScanBySingleScanner + 1);
 
 def getSrcFileName():
     return "rotate.pdf";
@@ -58,14 +64,52 @@ def getimgPath():
 def getStatus():
     return "PROCESSED"
 
+def createNameOrCodeByRandomization(length, builderCharacterType):
+    index = 0;
+    codeOrNameString = ""
+    while index < length:
+        if builderCharacterType == "Alpha":
+            rand = random.randrange(0,25)
+            codeOrNameString += str(chr(65+rand));
+        elif builderCharacterType == "Numeric":
+            rand = random.randrange(0,9)
+            codeOrNameString += str(chr(48+rand));
+        index = index + 1
+    return codeOrNameString;
 
-for index in range(1, 1000):
+def createValScoreDic(char):
+    dic = {}
+    dic['val'] = char
+    dic['score'] = getRandomConfidence();
+    return dic;
+
+
+def getIcrCharListFromOriginal(original):
+    valScoreDicList = []
+    for index in range(0,len(original)):
+        char = original[index : index+1]
+        valScoreDicList.append(createValScoreDic(char))
+    return valScoreDicList
+
+def getIcrcharImgpathOriginalDic(length, charOption):
+    innderDictionary = {}
+    innderDictionary['original'] = createNameOrCodeByRandomization(length, charOption);
+    innderDictionary['imgPath'] = getimgPath();
+    innderDictionary['icrChars'] = getIcrCharListFromOriginal(innderDictionary['original']);
+    return innderDictionary
+
+def getSchoolCode(length, charOptioin):
+    localDictionary = {}
+    localDictionary['value'] = getIcrcharImgpathOriginalDic(length, charOptioin);
+    return localDictionary;
+
+for index in range(0, 1000):
     dictionary = {"_class" : "com.progoti.kycreader.server.core.domains.DisbursementSheet"}
     dictionary['scannerId'] = getScannerId(index);
     dictionary['seq'] = getSeq(index);
     dictionary['srcFileName'] = getSrcFileName()
     dictionary['fullImg'] = getFullImg();
-    dictionary['status'] = getStatus()
-
+    dictionary['status'] = getStatus();
+    dictionary['schoolCode'] = getSchoolCode(12, CharOption.Numeric);
 
     print dictionary;
